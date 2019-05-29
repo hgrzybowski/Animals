32995e52b4e2fc64ef936c7b3a4529730fda5a94
hgrzybowski <h-grzybowski@wp.pl>
2019-05-29 12:35:43 +0200

    addSeeding

---
+++ Animals/Data/Migrations/20190529091540_Seeding.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Animals.Data.Migrations
+{
+    public partial class Seeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+  
+            migrationBuilder.Sql("INSERT INTO SpecialTags (Name) VALUES ('siodlo')");
+            migrationBuilder.Sql("INSERT INTO ProductTypes (Name) VALUES ('arab')");
+            migrationBuilder.Sql("INSERT INTO SpecialTags (Name) VALUES ('zaprzęg')");
+            migrationBuilder.Sql("INSERT INTO ProductTypes (Name) VALUES ('krwi angielskiej')");
+
+            //migrationBuilder.Sql("INSERT INTO Products (Name,Price,Available,ProductTypeId,SpecialTagsID) VALUES ('Karino',2,1,1,1)");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DLETE FROM ProductTypes");
+            
+            migrationBuilder.Sql("DLETE FROM SpecialTags");
+            //migrationBuilder.Sql("DLETE FROM Products");
+        }
+    }
+}
