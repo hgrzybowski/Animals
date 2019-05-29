ca6abc4f372340d7703ef538cb16ca75874083b1
hgrzybowski <h-grzybowski@wp.pl>
2019-05-29 18:52:23 +0200

    addSeedingAppointment

--- Animals/Data/Migrations/20190529091540_Seeding.cs
+++ Animals/Data/Migrations/20190529091540_Seeding.cs
@@ -12,9 +12,15 @@ namespace Animals.Data.Migrations
             migrationBuilder.Sql("INSERT INTO SpecialTags (Name) VALUES ('zaprzęg')");
             migrationBuilder.Sql("INSERT INTO ProductTypes (Name) VALUES ('krwi angielskiej')");
 
+
+
             migrationBuilder.Sql("INSERT INTO Products (Name,Price,Available,ShadeColor,ProductTypeId,SpecialTagsID) VALUES ('Karino',2222,1,'gray',1,1)");
             migrationBuilder.Sql("INSERT INTO Products (Name,Price,Available,ShadeColor,ProductTypeId,SpecialTagsID) VALUES ('Elpasso',5555,1,'biały',1,2)");
             migrationBuilder.Sql("INSERT INTO Products (Name,Price,Available,ShadeColor,ProductTypeId,SpecialTagsID) VALUES ('Opty',2545,1,'szary',2,1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','szary',22222,'g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','henryk',22222,'d21g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','magda',22222,'c31g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','ja',22222,'b41g@gmail.com',1)");
 
         }
 
@@ -24,6 +30,7 @@ namespace Animals.Data.Migrations
             
             migrationBuilder.Sql("DLETE FROM SpecialTags");
             migrationBuilder.Sql("DLETE FROM Products");
+            migrationBuilder.Sql("DLETE FROM Appointments");
         }
     }
 }
