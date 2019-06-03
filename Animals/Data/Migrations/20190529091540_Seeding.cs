3b037b1259de4531c5703bcd173cad721bf5cd03
hgrzybowski <h-grzybowski@wp.pl>
2019-06-03 10:20:57 +0200

    addSession

--- Animals/Data/Migrations/20190529091540_Seeding.cs
+++ Animals/Data/Migrations/20190529091540_Seeding.cs
@@ -17,10 +17,23 @@ namespace Animals.Data.Migrations
             migrationBuilder.Sql("INSERT INTO Products (Name,Price,Available,ShadeColor,ProductTypeId,SpecialTagsID) VALUES ('Karino',2222,1,'gray',1,1)");
             migrationBuilder.Sql("INSERT INTO Products (Name,Price,Available,ShadeColor,ProductTypeId,SpecialTagsID) VALUES ('Elpasso',5555,1,'biały',1,2)");
             migrationBuilder.Sql("INSERT INTO Products (Name,Price,Available,ShadeColor,ProductTypeId,SpecialTagsID) VALUES ('Opty',2545,1,'szary',2,1)");
-            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','szary',22222,'g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','magda',22222,'g@gmail.com',1)");
             migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','henryk',22222,'d21g@gmail.com',1)");
-            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','magda',22222,'c31g@gmail.com',1)");
-            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','ja',22222,'b41g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','domi',22222,'c31g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','gabi',22222,'b41g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','wiki',22222,'g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','ABC',22222,'d21g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','DEF',22222,'c31g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','JGH',22222,'b41g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','magda',22222,'g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','henryk',22222,'d21g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','domi',22222,'c31g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','gabi',22222,'b41g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','wiki',22222,'g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','ABC',22222,'d21g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','DEF',22222,'c31g@gmail.com',1)");
+            migrationBuilder.Sql("INSERT INTO Appointments (AppointmentDate,CustomerName,CustomerPhoneNumber,CustomerEmail,isConfirmed) VALUES ('2019-05-27','JGH',22222,'b41g@gmail.com',1)");
+
 
         }
 
